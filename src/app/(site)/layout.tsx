318cea76d6ad8c11d1ac34dca1c5f3bc01b0c995
miladxyz <milad.bhzz@gmail.com>
2026-06-21 13:23:52 +0330

    add speed insight

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -4,6 +4,7 @@ import { Header } from '@/components/Header'
 import { Footer } from '@/components/Footer'
 import { AuthProvider } from '@/context/AuthContext'
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import React from 'react'
 
 export const metadata: Metadata = {
@@ -30,6 +31,7 @@ export default function SiteLayout({ children }: { children: React.ReactNode }) 
           <Header />
           <main className="min-h-screen">{children}</main>
           <Analytics />
+          <SpeedInsights />
           <Footer />
         </AuthProvider>
       </body>
