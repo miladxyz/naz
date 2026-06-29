76669e33b8ad1916a2a9885cb40049e5c62287b0
miladxyz <milad.bhzz@gmail.com>
2026-06-29 11:22:01 +0330

    fix posts categories

--- src/app/(site)/dashboard/page.tsx
+++ src/app/(site)/dashboard/page.tsx
@@ -19,7 +19,7 @@ const categoryLabels: Record<string, string> = {
   family:      'حقوق خانواده',
   banking:     'حقوق بانکی',
   insurance:   'حقوق بیمه',
-  others: 'سایر',
+  others:      'سایر',
 }
 
 export default function DashboardPage() {
